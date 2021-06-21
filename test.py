20e993fad36c2d1924855bb2a252bd656ed7763c
Shriyans Gandhi <shri30yans@gmail.com>
2021-06-21 22:21:31 +0530

    Bug_fixes

--- test.py
+++ test.py
@@ -1,28 +1,3 @@
-import requests,lxml.html,random,time
-
-def get_page_html(url,headers_list=[{"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}]):
-        headers = random.choice(headers_list)
-        page = requests.get(url, headers=headers)
-        return page.content
-
-def scrape_scdigital(scdigital_link):
-    while True:
-        page_html = str(get_page_html(scdigital_link))
-        doc = lxml.html.fromstring(page_html)
-        #print(doc)
-        try:
-            stock = doc.xpath('//*[@id="notify_btn_div"]')                                  
-            add_to_cart_button = doc.xpath('//*[@id="product-add-to-cart"]')
-        except:
-            print("shopatscdigital Error")
-            continue
-        
-        print("Stock",stock)
-        print("Add_to_cart",add_to_cart_button)
-        time.sleep(1)
-
-
-scrape_scdigital("https://shopatsc.com/collections/playstation-5/products/playstation5-digital-edition")
-#scrape_scdigital("https://shopatsc.com/collections/home-audio/products/sony-ht-rt3-sound-bar-type-home-theatre-system-black")
-
+l=[]
+print()
 
